d7c04f6de6793d0f56461fe217d46683cc57cf4b
Edward Dupre <edwd42@gmail.com>
2018-09-29 19:26:34 -0400

    add ScraperController to do web scraping

--- src/main/java/net/ed/scraper/controller/HomeController.java
+++ src/main/java/net/ed/scraper/controller/HomeController.java
@@ -3,8 +3,6 @@ package net.ed.scraper.controller;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +21,6 @@ public class HomeController {
 	
 //	private static WebDriver driver;
 	
-//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -31,7 +28,6 @@ public class HomeController {
 //	@ResponseBody
 	@RequestMapping("/")
 	public String home(Locale locale, Model model) {
-//		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -39,13 +35,16 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate);
+		
+		ScraperController.scraperDriver();
+		
 		return "home";
 	}
 	
 	@RequestMapping("/scraper")
 	public String scraper(Model model) {
 
-		System.out.println("56. inside /scraper");
+		System.out.println("44. inside /scraper");
 
 		Totals t = new Totals();
 		t.setCurrentMarketValue("10,000");
@@ -53,6 +52,9 @@ public class HomeController {
 		t.setTotalGain("100");
 		
 		model.addAttribute("todays_totals", t);
+		
+		ScraperController.scraperDriver();
+		
         return "scraper";
 	}
 	
---
+++ src/main/java/net/ed/scraper/controller/ScraperController.java
@@ -0,0 +1,48 @@
+package net.ed.scraper.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ScraperController {
+	
+	public static void scraperDriver() {
+		
+		// add support for chrome driver
+		String keyx = "webdriver.chrome.driver";
+		String valuex = "/Users/melocal/Applications/lib/chromedriver";
+		System.setProperty(keyx, valuex);
+		
+		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors");
+		WebDriver driver = new ChromeDriver(options);
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
+		driver.get("https://google.com");
+		driver.findElement(By.name("q")).sendKeys("selenium");
+		
+		WebElement button = driver.findElement(By.name("btnK"));
+		button.sendKeys(Keys.ENTER);
+		
+        System.out.println("42. fin");
+        
+        driver.close();
+        driver.quit();
+        
+	
+	}
+
+}
