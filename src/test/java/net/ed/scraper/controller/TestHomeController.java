461485d43dcfcefb2ec5eaf822e71f4d5777fc8a
Edward Dupre <edwd42@gmail.com>
2018-09-29 21:34:31 -0400

    add test class TestHomeController and test verifyHomepageTitle()

---
+++ src/test/java/net/ed/scraper/controller/TestHomeController.java
@@ -0,0 +1,42 @@
+package net.ed.scraper.controller;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestHomeController {
+	
+	public WebDriver driver;
+	public String baseUrl = "http://localhost:9090/aws-selenium-scraper/";
+	
+	@BeforeTest
+	public void launchBrowser() {
+		// add support for chrome driver
+		String keyx = "webdriver.chrome.driver";
+		String valuex = "/Users/melocal/Applications/lib/chromedriver";
+		System.setProperty(keyx, valuex);
+		
+		ChromeOptions options = new ChromeOptions();
+		driver = new ChromeDriver(options);
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		
+		driver.get(baseUrl);
+	}
+	
+	@Test
+	public void verifyHomepageTitle() {
+		String expected = "Selenium Scraper";
+		String actual = driver.getTitle();
+		System.out.println(actual);
+		Assert.assertEquals(actual, expected);
+		
+	}
+}
